f2644c9dbec3fa1dd15412582339642c94e8b718
pilligrim28 <balu-133@ya.ru>
2022-07-27 22:10:28 +0300

    Testing for Log in

--- cypress/e2e/test-lanit_1.cy.js
+++ cypress/e2e/test-lanit_1.cy.js
@@ -1,10 +1,9 @@
 describe('Log in', function() {
   it('Sing in', function (){
     cy.visit('https://dev.ltdo.xyz/')
-   // cy.contains("Log in with your password").click()
-    cy.get('input[type="text"]').type('pilligrim28')
+    cy.get('input[type="email"]').type('pilligrim28')
     cy.get('input[type="password"]').type('40I65hs#')
-    cy.get('input[type="submit"]').click()
-    //cy.visit('https://teacher.skyeng.ru/teacherCabinet')
+    cy.get('span[class="mat-button-wrapper"]').click()
+    
   })
 })
