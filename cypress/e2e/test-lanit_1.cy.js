50cf1570d319926b1497f2b18782cd6d703c936e
pilligrim28 <balu-133@ya.ru>
2022-07-28 22:38:46 +0300

    testing for click svg

--- cypress/e2e/test-lanit_1.cy.js
+++ cypress/e2e/test-lanit_1.cy.js
@@ -3,8 +3,8 @@ describe('Log in', function() {
     cy.visit('https://dev.ltdo.xyz/')
     cy.get('input[type="email"]').type('pilligrim28')
     cy.get('input[type="password"]').type('40I65hs#{enter}')
-    //cy.visit('https://dev.ltdo.xyz/departments')
-    cy.get('input[ng-reflect-svg-icon="departments"]').click()
+    cy.visit('https://dev.ltdo.xyz/departments')
+   //cy.get('input[ng-reflect-svg-icon="departments"]').click()
     
   })
 })
