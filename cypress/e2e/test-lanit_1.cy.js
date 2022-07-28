11dbf7d591de8693ebaa1ccd8b20a25bc0b159dd
pilligrim28 <balu-133@ya.ru>
2022-07-28 23:05:31 +0300

    wait

--- cypress/e2e/test-lanit_1.cy.js
+++ cypress/e2e/test-lanit_1.cy.js
@@ -2,9 +2,9 @@ describe('Log in', function() {
   it('Sing in', function (){
     cy.visit('https://dev.ltdo.xyz/')
     cy.get('input[type="email"]').type('pilligrim28')
-    cy.get('input[type="password"]').type('40I65hs#{enter}')
-    cy.visit('https://dev.ltdo.xyz/departments')
-   //cy.get('input[ng-reflect-svg-icon="departments"]').click()
+    cy.get('input[type="password"]').type('40I65hs#{enter}').wait(10000)
+    //cy.visit('https://dev.ltdo.xyz/departments')
+    cy.contains('Департаменты').click()
     
   })
 })
