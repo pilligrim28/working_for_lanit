a1968014bb9b4057313afe87c96e9c736c66c6c3
pilligrim28 <balu-133@ya.ru>
2022-07-30 20:26:44 +0300

    new autorization

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -4,7 +4,9 @@ describe('Log in', function() {
     cy.get('input[type="email"]').type('pilligrim28')
     cy.get('input[type="password"]').type('40I65hs#{enter}').wait(10000)
     //cy.visit('https://dev.ltdo.xyz/departments')
-    cy.contains('Департаменты').click()
+    //cy.contains('Департаменты').click()
+    cy.get("a[ng-reflect-router-link='departments']").click()
+    cy.contains('+ Создать департамент').click()
     
   })
 })
