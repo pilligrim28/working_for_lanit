e1a5684fa89bac01bfa960368fef1b39d66b9a53
pilligrim28 <balu-133@ya.ru>
2022-07-31 14:22:15 +0300

    smoke test #1

--- cypress/e2e/testing_for_smoke.cy.js
+++ cypress/e2e/testing_for_smoke.cy.js
@@ -17,7 +17,8 @@ describe('Log in', function() {
     cy.get('input[type="text"]').type("QA Departments")
     cy.get("span[class='mat-select-placeholder mat-select-min-line ng-tns-c196-21 ng-star-inserted']").click() //Добавление нового директора
     cy.get('.mat-option-text').contains('Николаев Александр').click()
-    cy.get('textarea[type="text"]').type("Мы самые скромные ребята, нас любят даже утята")
+    cy.get('textarea[class="mat-input-element mat-form-field-autofill-control ng-tns-c161-22 ng-untouched ng-pristine ng-valid cdk-text-field-autofill-monitored"]').type("Мы самые скромные ребята, нас любят даже утята")
+    cy.get('button[class="mat-focus-indicator mat-flat-button mat-button-base mat-primary"]').click()
 
   
  })
